452eaccd0edc00f38e12d50c22bfde25bb0f6ecf
horler408 <horler.web@gmail.com>
2021-01-19 14:09:14 +0100

    profile with pagination

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,10 +1,37 @@
-import React from 'react'
+import React from 'react';
+import { Card, ListGroup } from 'react-bootstrap';
+//import {Grid, Card, Typography} from '@material-ui/core';
 
 const ProfileCard = ({user})=> {
     return (
-        <div className="">
-            <h3>{`${user.FirstName} ${user.LastName}`}</h3>
-        </div>
+        <Card className="m-4" style={{ width: '25rem'}}>
+            <Card.Header>{`${user.FirstName} ${user.LastName}`}</Card.Header>
+            <Card.Body>
+                <div>
+                    <img className="image" src="https://via.placeholder.com/150" alt={user.URL} />
+                </div>
+                <div>
+                    <div>
+                        <Card.Title>
+                            {`${user.FirstName} ${user.LastName}`}
+                        </Card.Title>
+                    </div>
+                </div>
+                <ListGroup className="d-flex justify-content-between">
+                        <ListGroup.Item>User Name - <span className="font-weight-bold">{user.UserName}</span></ListGroup.Item>
+                        <ListGroup.Item>Credit Card Number - <span className="font-weight-bold">{user.CreditCardNumber}</span></ListGroup.Item>
+                        <ListGroup.Item>Credit Card Type - <span className="font-weight-bold">{user.CreditCardType}</span></ListGroup.Item>
+                        <ListGroup.Item>Payment Method - <span className="font-weight-bold">{user.PaymentMethod}</span></ListGroup.Item>
+                        <ListGroup.Item>Email - <span className="font-weight-bold">{user.Email}</span></ListGroup.Item>
+                        <ListGroup.Item>Gender - <span className="font-weight-bold">{user.Gender}</span></ListGroup.Item>
+                        <ListGroup.Item>Last Login - <span className="font-weight-bold">{user.LastLogin}</span></ListGroup.Item>
+                        <ListGroup.Item>Location - <span className="font-weight-bold">{`${user.Latitude}, ${user.Longitude}`}</span></ListGroup.Item>
+                        <ListGroup.Item>Mac Address - <span className="font-weight-bold">{user.MacAddress}</span></ListGroup.Item>
+                        <ListGroup.Item>Phone Number - <span className="font-weight-bold">{user.PhoneNumber}</span></ListGroup.Item>
+                        <ListGroup.Item>Website - <span className="font-weight-bold"><Card.Link href="#">{user.URL}</Card.Link></span></ListGroup.Item>
+                    </ListGroup>
+            </Card.Body>
+        </Card>
     )
 }
 
--- src/components/Profiles.js
+++ src/components/Profiles.js
@@ -1,24 +1,29 @@
 import React, { useState, useEffect } from 'react';
-import ProfileCard from './ProfileCard'
+import Axios from 'axios';
+import ProfileCard from './ProfileCard';
+import Pagination from './Pagination';
+import SearchForm from './SearchForm';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const Profiles = () => {
     const[users, setUsers] = useState([])
     const[loading, setLoading] = useState(false)
-    const[count, setCount] = useState("")
+    const[page, setPage] = useState(1)
+    const[usersPerPage] = useState(20)
     const baseURL = `https://api.enye.tech/v1/challenge/records`
 
     useEffect(() => {
         const getUsers = async () => {
             try {
                 setLoading(true)
-                const response = await fetch(baseURL)
-                const data = await response.json();
+                const response = await Axios.get(baseURL)
+                const data = await response.data
                 const results = await data.records.profiles
-                //console.log(results);
+                console.log(results);
                 setUsers(results);
-                setCount(data.size)
                 setLoading(false)
             } catch(err) {
+                if(Axios.isCancel(err)) return
                 console.log(err.message);
                 setLoading(false)
             }
@@ -27,17 +32,32 @@ const Profiles = () => {
         getUsers();
     }, [baseURL])
 
-    return (
-        <div className="">
-            {loading && <div>Loading...</div>}
-            <h2>{count} users available</h2>
-            <div>
-                {users.map((user, i) => (
-                    <ProfileCard key={i} user={user} />
-                ))}
-            </div>
+    const indexOfLastUser = page * usersPerPage;
+    const indexOfFirstUser = indexOfLastUser - usersPerPage;
+    const currentUsers = users.slice(indexOfFirstUser, indexOfLastUser);
 
-        </div>
+    const paginate = pageNumber => setPage(pageNumber);
+
+    return (
+        <>
+            {loading && <div>Loading...</div>}
+            <Pagination 
+                usersPerPage={usersPerPage}
+                totalUsers={users.length}
+                paginate={paginate}
+            />
+            <SearchForm />
+            <Container className="mt-5 d-flex flex-wrap">
+
+                {currentUsers.map((user, i) => (
+                    <Row>
+                        <Col md={6}>
+                            <ProfileCard key={i} user={user} />
+                        </Col>
+                    </Row>
+                ))}
+            </Container>
+        </>
     )
 }
 
